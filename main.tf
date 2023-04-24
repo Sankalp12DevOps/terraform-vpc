a9693505bc6eff324fde95625bfae307ea691a95
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-24 20:27:58 +0530

    m

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 module "vpc"{
-source       =    "./vendor/modules/vpc"
-VPC_CIDR     =    var.VPC_CIDR
-ENV          =    var.ENV
-AZ           =    var.AZ
-PRVT_SUBNET  =    var.PRVT_SUBNET
-PUBLC_SUBNET =    var.PUBLC_SUBNET
+source           =    "./vendor/modules/vpc"
+VPC_CIDR         =    var.VPC_CIDR
+ENV              =    var.ENV
+AZ               =    var.AZ
+PRVT_SUBNET      =    var.PRVT_SUBNET
+PUBLC_SUBNET     =    var.PUBLC_SUBNET
+DEFAULT_RT       =    var.DEFAULT_RT
+ALLOW_ALL_CIDR   =    var.ALLOW_ALL_CIDR
+DEFAULT_CIDR     =    var.DEFAULT_CIDR
+PEER_VPC_ID      =    var.PEER_VPC_ID
 
 
 }
