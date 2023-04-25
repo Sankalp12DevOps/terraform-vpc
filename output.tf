1b82816437d33522153b92a21076f1c934209e0a
Sankalp12DevOps <sravanthi388@gmail.com>
2023-04-25 16:22:59 +0530

    m

--- output.tf
+++ output.tf
@@ -1,27 +1,27 @@
 output defaultRoute_id{
-value =var.DEFAULT_RT
+value = var.DEFAULT_RT
 }
 
 output defaultcidr{
-value =var.DEFAULT_CIDR
+value = var.DEFAULT_CIDR
 }
 
 
 output peer_vpc_id{
-value =var.PEER_VPC_ID
+value = var.PEER_VPC_ID
 }
 
 output publicSubnet_cidrs{
-value =var.PUBLC_SUBNET
+value = var.PUBLC_SUBNET
 }
 
 output privateSubnet_cidrs{
-value =var.PRVT_SUBNET
+value = var.PRVT_SUBNET
 }
 output env{
-value =var.ENV
+value = var.ENV
 }
 
 output vpc_cidr{
-value =var.VPC_CIDR
+value = var.VPC_CIDR
 }
